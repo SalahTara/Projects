6d433946c7364a39d1fd01209eb77810ab4e086e
SalahTara <salahtarabichi@gmail.com>
2023-12-23 10:47:59 -0500

    Fresh Start

--- Practice/ex1.py
+++
@@ -1,8 +0,0 @@
-def add(num1: int, num2: int) -> str:
-    multiply = num1 * num2
-    sum = num1 + num2
-    if multiply <= 1000:
-        return ("The result is " + str(multiply))
-    return ("The result is " + str(sum))
-
-print(add(40,30))
