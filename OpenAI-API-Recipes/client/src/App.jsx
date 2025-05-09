2e6a05f4634d27144dd673c65250d9933dde782d
Salah <altarabs@mcmaster.ca>
2025-05-09 19:11:35 +0300

    Design frontend form submission

--- OpenAI-API-Recipes/client/src/App.jsx
+++ OpenAI-API-Recipes/client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./index.module.css";
+import CookingLogo from "./assets/cooking.png";
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [recipeDescription, setRecipeDescription] = useState();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted", recipeDescription);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={styles.main}>
+      <img src={CookingLogo} alt="" className={styles.icon} />
+      <h3>Generate A Recipe With whatever's in your fridge</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-descrption"
+          placeholder="Describe What's inside your fridge"
+          onChange={(e) => setRecipeDescription(e.target.value)}
+        ></input>
+        <input type="submit" value="Generate Recipe" />
+      </form>
+    </main>
+  );
 }
 
-export default App
+export default App;
