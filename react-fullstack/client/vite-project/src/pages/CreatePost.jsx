1538cacea8ebf451cb830d6ec37e16056f344199
Salah <altarabs@mcmaster.ca>
2025-07-18 10:21:23 +0300

    Add Page Not Found & Fix CSS

--- react-fullstack/client/vite-project/src/pages/CreatePost.jsx
+++ react-fullstack/client/vite-project/src/pages/CreatePost.jsx
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../helpers/AuthContext";
 
 function createpost() {
+  const { authState } = useContext(AuthContext);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authState.status) {
+      navigate("/login");
+    }
+  }, []);
   const initialValues = {
     title: "",
     postText: "",
-    username: "",
   };
   const onSubmit = (data) => {
-    axios.post("http://localhost:3001/posts", data).then((response) => {
-      navigate("/");
-    });
+    axios
+      .post("http://localhost:3001/posts", data, {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then((response) => {
+        navigate("/");
+      });
   };
-
-  let navigate = useNavigate();
 
   const validationSchema = Yup.object().shape({
     title: Yup.string().required("You must have a title"),
     postText: Yup.string().required(),
-    username: Yup.string().min(3).max(15).required(),
   });
+
   return (
     <div className="createPostPage">
       <Formik
@@ -49,15 +59,7 @@ function createpost() {
             name="postText"
             placeholder="(Ex. Hi I'm Johm...)"
           />
-          <label>Username: </label>
-          <ErrorMessage name="username-error" component="span" />
-          <Field
-            className="field"
-            autoComplete="off"
-            id="inputCreatePost"
-            name="username"
-            placeholder="(Ex. John...)"
-          />
+
           <button type="submit">Create Post</button>
         </Form>
       </Formik>
--- react-fullstack/client/vite-project/src/pages/Home.jsx
+++ react-fullstack/client/vite-project/src/pages/Home.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import { AuthContext } from "../helpers/AuthContext";
 
 function Home() {
-  const [listofPosts, setListOfPosts] = useState([]);
+  const [listOfPosts, setListOfPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
+  const { authState } = useContext(AuthContext);
   let navigate = useNavigate();
 
   useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setListOfPosts(response.data);
-    });
+    if (!authState.status) {
+      navigate("/login");
+    } else {
+      axios
+        .get("http://localhost:3001/posts", {
+          headers: { accessToken: localStorage.getItem("accessToken") },
+        })
+        .then((response) => {
+          setListOfPosts(response.data.listOfPosts);
+          setLikedPosts(
+            response.data.likedPosts.map((like) => {
+              return like.PostId;
+            })
+          );
+        });
+    }
   }, []);
 
   const likeAPost = (postId) => {
@@ -22,7 +39,7 @@ function Home() {
       )
       .then((response) => {
         setListOfPosts(
-          listofPosts.map((post) => {
+          listOfPosts.map((post) => {
             if (post.id === postId) {
               if (response.data.liked === true) {
                 return { ...post, Likes: [...post.Likes, 0] };
@@ -36,12 +53,21 @@ function Home() {
             }
           })
         );
+        if (likedPosts.includes(postId)) {
+          setLikedPosts(
+            likedPosts.filter((id) => {
+              return id != postId;
+            })
+          );
+        } else {
+          setLikedPosts([...likedPosts, postId]);
+        }
       });
   };
 
   return (
     <div className="App">
-      {listofPosts.map((value, key) => {
+      {listOfPosts.map((value, key) => {
         return (
           <div className="post">
             <div className="title">{value.title}</div>
@@ -54,15 +80,19 @@ function Home() {
               {value.postText}
             </div>
             <div className="footer">
-              {value.username}
-              <button
-                onClick={() => {
-                  likeAPost(value.id);
-                }}
-              >
-                Like
-              </button>
-              <label>{value.Likes.length}</label>
+              <div className="username">{value.username}</div>
+              <div className="buttons">
+                <ThumbUpAltIcon
+                  onClick={() => {
+                    likeAPost(value.id);
+                  }}
+                  className={
+                    likedPosts.includes(value.id) ? "unlikeBttn" : "likeBttn"
+                  }
+                />
+
+                <label>{value.Likes.length}</label>
+              </div>
             </div>
           </div>
         );
