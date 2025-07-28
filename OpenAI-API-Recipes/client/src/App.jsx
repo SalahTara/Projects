f641cb4c8638fe53798c0115c7c88ffe95a369a2
Salaheddin Altarabichi <salahtarabichi@gmail.com>
2025-07-28 18:41:43 +0300

    feat: Add Query History

--- OpenAI-API-Recipes/client/src/App.jsx
+++ OpenAI-API-Recipes/client/src/App.jsx
@@ -1,45 +1,92 @@
 import styles from "./styles.module.css";
 import sqlServer from "./assets/sql-server.png";
 import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
   const [userPrompt, setUserPrompt] = useState("");
   const [sqlQuery, setSqlQuery] = useState("");
+  const [listOfPosts, setListOfPosts] = useState([]);
+
+  const postQueries = async (promptText, queryText) => {
+    try {
+      await axios.post("http://localhost:3005/queries", {
+        prompt: promptText,
+        query: queryText,
+      });
+    } catch (error) {
+      console.error("Error posting query:", error);
+    }
+  };
+
+  const getQueries = async () => {
+    try {
+      const response = await axios.get("http://localhost:3005/queries");
+      console.log(response);
+      setListOfPosts(response.data.listOfPosts);
+    } catch (error) {
+      console.error("Error fetching queries:", error);
+    }
+  };
+  useEffect(() => {
+    getQueries();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    const query = await generateQuery();
-    setSqlQuery(query);
+
+    const sqlQuery = await generateQuery();
+    setSqlQuery(sqlQuery);
+    console.log("Returned From Server: ", sqlQuery);
+
+    await postQueries(userPrompt, sqlQuery);
+    await getQueries();
   };
 
   const generateQuery = async () => {
-    const response = await fetch("http://localhost:3002/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ queryDescription: userPrompt }),
-    });
+    try {
+      const response = await axios.post("http://localhost:3005/generate", {
+        queryDescription: userPrompt,
+      });
 
-    const data = await response.json();
-    return data.sqlQuery.trim();
+      return response.data.response.trim();
+    } catch (error) {
+      console.error("Error generating query:", error);
+    }
   };
 
   return (
     <main className={styles.main}>
-      <img src={sqlServer} className={styles.icon} alt="SQL server" />
-      <h3>Generate SQL</h3>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="query-description"
-          placeholder="Describe your query"
-          value={userPrompt}
-          onChange={(e) => setUserPrompt(e.target.value)}
-        />
-        <input type="submit" value="Generate query" />
-      </form>
-      <pre>{sqlQuery}</pre>
+      <div className="leftContainer ">
+        <div className="test">
+          {listOfPosts.map((value, key) => {
+            return (
+              <div className="queryContainer">
+                <div>{value.prompt}</div>
+                <div>{value.query}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="centerContainer">
+        <img src={sqlServer} className={styles.icon} alt="SQL server" />
+        <h3>Generate SQL</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="query-description"
+            placeholder="Describe your query"
+            value={userPrompt}
+            onChange={(e) => setUserPrompt(e.target.value)}
+            required
+          />
+          <input type="submit" value="Generate query" />
+        </form>
+        <pre>{sqlQuery}</pre>
+      </div>
+      <div className="rightContainer"></div>
     </main>
   );
 }
