86bf46abbdb6c1313005638888b8752b86c1aa01
Salaheddin Altarabichi <salahtarabichi@gmail.com>
2025-08-10 15:40:32 +0300

    Change Project Name

--- java/Mystery.java
+++
@@ -1,8 +0,0 @@
-public class Mystery {
-	public Mystery(int w, int x, int y, int z) {
-		System.out.println(x);
-	}
-		public static void main(String[] args) {
-		Mystery q = new Mystery(0, 123, 0, 0);
-	}
-}
--- java/kgToLbs.java
+++
@@ -1,8 +0,0 @@
-public class kgToLbs {
-	public static void main(String[] args) {
-		double numOfLbs = 200d;
-		double result = numOfLbs * (0.45359237d);
-		System.out.println(result);
-
-	}
-}
