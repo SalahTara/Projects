507d2930e1bdddbaf347ebf9cb972ebb23e6994a
Salah <salahtarabichi@gmail.com>
2025-09-24 18:08:50 -0400

    Add Navbar

--- voice-recorder-app/client/src/App.jsx
+++ voice-recorder-app/client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MicIcon from "../src/assets/microphone.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="Main bg-gray-300 min-h-screen">
+      <div className="NavbarContainer bg-gradient-to-r from-gray-800 to-gray-600 text-white px-6 py-4 shadow-lg flex justify-between items-center">
+        {/* Left Side */}
+        <button className="hover:cursor-pointer">
+          <div className="homeContainer flex justify-center items-center">
+            <img className="w-auto h-9" src={MicIcon}></img>
+            <div className="pl-2 font-mono">Voicely</div>
+          </div>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {/* Right Side */}
+        <div className="loginContainer font-mono">
+          <button className="hover:cursor-pointer">Login</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
